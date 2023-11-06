0a6af791b856b5574481d5569b0d6b721991b2e9
Marco Minerva <marco.minerva@gmail.com>
2023-11-06 17:16:51 +0100

    Add Directory.Build.props

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -1,4 +1,35 @@
-﻿using ConsoleApp.BusinessLayer;
+﻿using System.Globalization;
+using ConsoleApp.BusinessLayer;
+
+var date = DateOnly.ParseExact("29/11/2023", ["dd/MM/yyyy", "dd-MM-yyyy"], CultureInfo.InvariantCulture, DateTimeStyles.None);
+Console.WriteLine(date);
+
+IList<double> list = [1, 2.3, 3, 3];
+list.Add(12);
+var list2 = list.AsReadOnly();
+list.Add(1);
+
+Foo(list);
+
+TimeProvider timeProvider = TimeProvider.System;
+
+Microsoft.Extensions.Time.Testing.FakeTimeProvider faker = new Microsoft.Extensions.Time.Testing.FakeTimeProvider();
+faker.SetUtcNow(new DateTimeOffset(2020, 1, 1, 1, 1, 1, TimeSpan.Zero));
+
+Console.WriteLine(faker.GetUtcNow().ToString());
+faker.SetUtcNow(new DateTimeOffset(2020, 1, 1, 1, 1, 2, TimeSpan.Zero));
+Console.WriteLine(faker.GetUtcNow().ToString());
+
 
 Result result = Service.Ok();
 Console.WriteLine(result.Message);
+
+void Foo(IList<double> list)
+{
+
+}
+
+public class Person(string firstName, string lastName)
+{
+}
+
