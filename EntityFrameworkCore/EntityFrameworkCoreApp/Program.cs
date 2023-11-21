67883fbfd8d0433a5c42a7a95c92298ac8496cff
Marco Minerva <marco.minerva@gmail.com>
2023-11-21 11:17:11 +0100

    Delete unused code

--- EntityFrameworkCore/EntityFrameworkCoreApp/Program.cs
+++ EntityFrameworkCore/EntityFrameworkCoreApp/Program.cs
@@ -1,5 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-using EntityFrameworkCoreApp;
+﻿using EntityFrameworkCoreApp;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Diagnostics;
 
@@ -9,15 +8,6 @@ Console.ReadLine();
 
 public partial class ApplicationDbContext : DbContext
 {
-    public ApplicationDbContext()
-    {
-    }
-
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-        : base(options)
-    {
-    }
-
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseSqlServer(Constants.ConnectionString);
